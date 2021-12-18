cb3b3d1ffbb2a9db47b87ca0a530f97977d2c8f4
ps173 <prathamsharma173@gmail.com>
2021-12-18 22:55:54 +0530

    Added idb keyval for persistent storage

--- src/components/Editor.tsx
+++ src/components/Editor.tsx
@@ -1,22 +1,55 @@
-import { ChangeEvent, useState } from "react";
+import { ChangeEvent, useEffect, useState } from "react";
+import { set, get } from "idb-keyval";
 
 const Editor = () => {
   const [editorValue, setEditorValue] = useState("Enter Your Text Here");
+
+  useEffect(() => {
+    const fetchState = async () => {
+      const editorState = await get("state");
+      if (editorState !== undefined) {
+        setEditorValue(JSON.parse(editorState));
+      }
+    };
+    fetchState();
+  }, []);
+
+  const saveState = async () => {
+    await set("state", JSON.stringify(editorValue));
+  };
+
   const handleChange = (event: ChangeEvent<HTMLTextAreaElement>) => {
     setEditorValue(event.target.value);
   };
+
   return (
-    <textarea
-      style={{
-        color: "white",
-        borderColor: "transparent",
-        width: "100%",
-        height: "100%",
-        resize: "none",
-      }}
-      value={editorValue}
-      onChange={handleChange}
-    />
+    <>
+      <textarea
+        style={{
+          color: "white",
+          borderColor: "transparent",
+          width: "100%",
+          height: "100%",
+          resize: "none",
+        }}
+        value={editorValue}
+        onChange={handleChange}
+      />
+      <button
+        className="btn-small"
+        style={{
+          background: "black",
+          color: "white",
+          position: "absolute",
+          top: "2%",
+          right: "2%",
+          transition: "0.3s",
+        }}
+        onClick={saveState}
+      >
+        Save
+      </button>
+    </>
   );
 };
 
